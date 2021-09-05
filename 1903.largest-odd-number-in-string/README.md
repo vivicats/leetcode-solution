3211e9aff55d7fed691e3914f6e3e2f322432cb0
Jeremy Cui <jeremy.cui@rokt.com>
2021-09-06 08:28:03 +1000

    Update 1903

---
+++ 1903.largest-odd-number-in-string/README.md
@@ -0,0 +1,9 @@
+# 1903. Largest Odd Number in String
+
+## Question
+
+https://leetcode.com/problems/largest-odd-number-in-string/
+
+## Solution
+
+Find the right most odd digit
---
+++ 1903.largest-odd-number-in-string/solution_1_pass.cs
@@ -0,0 +1,11 @@
+public string LargestOddNumber(string num)
+{
+    var endIndex = num.Length;
+
+    while (endIndex > 0 && num[endIndex - 1] % 2 == 0)
+    {
+        endIndex--;
+    }
+
+    return num.Substring(0, endIndex);
+}
